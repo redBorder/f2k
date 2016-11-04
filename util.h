642e34eb30c7a5d065fff5e6122a630992f86323
Eugenio Pérez <eupm90@gmail.com>
2016-11-04 09:33:51 +0000

    :bug: returning const bool

--- util.h
+++ util.h
@@ -280,10 +280,9 @@ typedef struct _IPNameAssoc NumNameAssoc;
 
 bool parseAddress(const char *address, netAddress_t *netaddress);
 
-static const bool safe_parse_address(const char *addr, netAddress_t *netAddress)
+static bool safe_parse_address(const char *addr, netAddress_t *netAddress)
                                                         __attribute__((unused));
-static const bool safe_parse_address(const char *addr,
-                                                    netAddress_t *netAddress) {
+static bool safe_parse_address(const char *addr, netAddress_t *netAddress) {
   char ipv6_buffer[INET6_ADDRSTRLEN + strlen("/128")];
   snprintf(ipv6_buffer,sizeof(ipv6_buffer),"%s",addr); // Need to copy
   return parseAddress(ipv6_buffer, netAddress);
