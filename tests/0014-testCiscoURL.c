7ec60f3d133e067833367320199ba6c4cf2e0739
Eugenio Pérez <eupm90@gmail.com>
2016-11-29 13:10:02 +0000

    Adapt tests/0014-testCiscoURL to new test system

--- tests/0014-testCiscoURL.c
+++ tests/0014-testCiscoURL.c
@@ -24,120 +24,60 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
-/// @todo template+flow in the same message
-struct TestV10Template{
-	IPFIXFlowHeader flowHeader;
-	IPFIXSet flowSetHeader;
-	V9TemplateDef templateHeader; /* It's the same */
-	const uint8_t templateBuffer[92];
-};
+#define TEST_IPFIX_FLOW_HEADER  \
+	.unix_secs = 0xdd5d6952, \
+	.flow_sequence = 0x38040000, \
+	.observationDomainId = 0x00010000,
 
-struct TestV10Flow{
-	IPFIXFlowHeader flowHeader;
-	IPFIXSet flowSetHeader;
-	const uint8_t buffer1[0x00ee - sizeof(IPFIXFlowHeader) - sizeof(IPFIXSet)];
-}__attribute__((packed));
+#define IPFIX_TEMPLATE_ID 0x0200
 
-static const struct TestV10Template v10Template = {
-	.flowHeader = {
-		/*uint16_t*/ .version = 0x0a00,           /* Current version=9*/
-		/*uint16_t*/ .len = 0x7400,           /* The number of records in PDU. */
-		/*uint32_t*/ .unix_secs = 0xdd5d6952,     /* Current time in msecs since router booted */
-		/*uint32_t*/ .flow_sequence = 0x38040000, /* Sequence number of total flows seen */
-		/*uint32_t*/ .observationDomainId = 0x00010000,      /* Source id */
-	},
+#define M_CISCO_URL 0x34, 0x03, 0x00, 0x00, 0x50, 0x34, 0x01, '/', \
+		    'p',  'r',  'o',  'f',  'i',  'l',  'e',  's', \
+		    '/',  'p',  'r',  'o',  'f',  'i',  'l',  'e', \
+		    '_',  '1',  '2',  '3',  '4',  '5',  '6',  '7', \
+		    '7',  '_',  '7',  '5',  's',  'q',  '_',  '1', \
+		    '1',  '2',  '3',  '4',  '5',  '6',  '7',  '8', \
+		    '2',  '.',  'j',  'p',  'g'
 
-	.flowSetHeader = {
-		/*uint16_t*/ .set_id = 0x0200,
-		/*uint16_t*/ .set_len = 0x6400,
-	},
+#define CISCO_HOST 0x1d, 0x03, 0x00, 0x00, 0x50, 0x34, 0x02, 'i', \
+		   'm',  'a',  'g',  'e',  's',  '.',  'a',  'k', \
+		   '.',  'i',  'n',  's',  't',  'a',  'g',  'r', \
+		   'a',  'm',  '.',  'c',  'o',  'm'
 
-	.templateHeader = {
-		/*uint16_t*/ .templateId = 0x0d01, /*269*/
-		/*uint16_t*/ .fieldCount = 0x1300,
-	},
+#define CISCO_UA 0x4e, 0x03, 0x00, 0x00, 0x50, 0x34, 0x03, 'I', \
+		 'n',  's',  't',  'a',  'g',  'r',  'a',  'm', \
+		 ' ',  '4',  '.',  '2',  '.',  '3',  ' ',  '(', \
+		 'i',  'P',  'h',  'o',  'n',  'e',  '5',  ',', \
+		 '1',  ';',  ' ',  'i',  'P',  'h',  'o',  'n', \
+		 'e',  ' ',  'O',  'S',  ' ',  '7',  '_',  '0', \
+		 '_',  '2',  ';',  ' ',  'e',  'n',  '_',  'U', \
+		 'S',  ';',  ' ',  'e',  'n',  ')',  ' ',  'A', \
+		 'p',  'p',  'l',  'e',  'W',  'e',  'b',  'K', \
+		 'i',  't',  '/',  '4',  '2',  '0',  '+'
 
-	.templateBuffer = {
-		0x00, 0x08, 0x00, 0x04, /* SRC ADDR */
-		0x00, 0x0c, 0x00, 0x04, /* DST ADDR */
-		0x00, 0x3c, 0x00, 0x01, /* IP VERSION */
-		0x00, 0x04, 0x00, 0x01, /* PROTO */
-		0x00, 0x07, 0x00, 0x02, /* SRC PORT */
-		0x00, 0x0b, 0x00, 0x02, /* DST PORT */
-		0x00, 0x88, 0x00, 0x01, /* flowEndreason */
-		0x00, 0xef, 0x00, 0x01, /* biflowDirection */
-		0x00, 0x30, 0x00, 0x01, /* FLOW_SAMPLER_ID */
-		0x01, 0x18, 0x00, 0x08, /* TRANSACTION_ID */
-		0x00, 0x5f, 0x00, 0x04, /* APPLICATION ID*/
-		0xaf, 0xcb, 0xff, 0xff, 0x00, 0x00, 0x00, 0x09, /* CISCO_URL, variable length */
-		0xaf, 0xcb, 0xff, 0xff, 0x00, 0x00, 0x00, 0x09, /* CISCO_URL, variable length */
-		0xaf, 0xcb, 0xff, 0xff, 0x00, 0x00, 0x00, 0x09, /* CISCO_URL, variable length */
-		0xaf, 0xcb, 0xff, 0xff, 0x00, 0x00, 0x00, 0x09, /* CISCO_URL, variable length */
-		0x00, 0x01, 0x00, 0x08, /* BYTES: */
-		0x00, 0x02, 0x00, 0x04, /* PKTS*/
-		0x00, 0x16, 0x00, 0x04, /* FIRST_SWITCHED */
-		0x00, 0x15, 0x00, 0x04, /* LAST_SWITCHED*/
-	}
-};
+#define CISCO_REFERER 0x06, 0x03, 0x00, 0x00, 0x50, 0x34, 0x04
 
-static const struct TestV10Flow v10Flow = {
-	.flowHeader = {
-		/*uint16_t*/ .version = 0x0a00,           /* Current version=9*/
-		/*uint16_t*/ .len = 0xee00,               /* The number of records in PDU. */
-		/*uint32_t*/ .unix_secs = 0xdd5d6952,     /* Current time in msecs since router booted */
-		/*uint32_t*/ .flow_sequence = 0x38040000, /* Sequence number of total flows seen */
-		/*uint32_t*/ .observationDomainId = 0x00010000,      /* Source id */
-	},
-
-	.flowSetHeader = {
-		/*uint16_t*/ .set_id = 0x0d01,
-		/*uint16_t*/ .set_len = 0xde00,
-	},
-
-	.buffer1 = {
-		0x0a, 0x0d, 0x7a, 0x2c, /* SRC ADDR 10.13.122.44 */
-		0x42, 0xdc, 0x98, 0x13, /* DST ADDR 66.220.152.19*/
-		0x04,                   /* IP VERSION: 4 */
-		0x06,                   /* PROTO: 6 */
-		0xd5, 0xb9,             /* SRC PORT: 54713 */
-		0x01, 0xbb,             /* DST PORT: 443 */
-		0x03,                   /* flowEndreason */
-		0x01,                   /* biflowDirection */
-		0x00,                   /* FLOW_SAMPLER_ID */
-		0x8f, 0x63, 0xf3, 0x40, 0x00, 0x01, 0x00, 0x00, /* TRANSACTION_ID */
-		0x03, 0x00, 0x00, 0x50, /* APPLICATION ID 13:453 */
-		/* CISCO HTTP URL */
-		0x34, 0x03, 0x00, 0x00, 0x50, 0x34, 0x01, 0x2f, /* ....P4./ */
-		0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65, 0x73, /* profiles */
-		0x2f, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65, /* /profile */
-		0x5f, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, /* _1234567 */
-		0x37, 0x5f, 0x37, 0x35, 0x73, 0x71, 0x5f, 0x31, /* 7_75sq_1 */
-		0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, /* 12345678 */
-		0x32, 0x2e, 0x6a, 0x70, 0x67,                   /* 2.jpg    */
-		/* CISCO HTTP HOST */
-		0x1d, 0x03, 0x00, 0x00, 0x50, 0x34, 0x02, 0x69,  /* ....P4.i */
-		0x6d, 0x61, 0x67, 0x65, 0x73, 0x2e, 0x61, 0x6b,  /* mages.ak */
-		0x2e, 0x69, 0x6e, 0x73, 0x74, 0x61, 0x67, 0x72,  /* .instagr */
-		0x61, 0x6d, 0x2e, 0x63, 0x6f, 0x6d,              /* am.com    */
-		/* CISCO HTTP USER-AGENT */
-		0x4e, 0x03, 0x00, 0x00, 0x50, 0x34, 0x03, 0x49,   /* ....P4.I */
-		0x6e, 0x73, 0x74, 0x61, 0x67, 0x72, 0x61, 0x6d,   /* nstagram */
-		0x20, 0x34, 0x2e, 0x32, 0x2e, 0x33, 0x20, 0x28,   /*  4.2.3 ( */
-		0x69, 0x50, 0x68, 0x6f, 0x6e, 0x65, 0x35, 0x2c,   /* iPhone5, */
-		0x31, 0x3b, 0x20, 0x69, 0x50, 0x68, 0x6f, 0x6e,   /* 1; iPhon */
-		0x65, 0x20, 0x4f, 0x53, 0x20, 0x37, 0x5f, 0x30,   /* e OS 7_0 */
-		0x5f, 0x32, 0x3b, 0x20, 0x65, 0x6e, 0x5f, 0x55,   /* _2; en_U */
-		0x53, 0x3b, 0x20, 0x65, 0x6e, 0x29, 0x20, 0x41,   /* S; en) A */
-		0x70, 0x70, 0x6c, 0x65, 0x57, 0x65, 0x62, 0x4b,   /* ppleWebK */
-		0x69, 0x74, 0x2f, 0x34, 0x32, 0x30, 0x2b,         /* it/420+  */
-		/* CISCO REFERER */
-		0x06, 0x03, 0x00, 0x00, 0x50, 0x34, 0x04,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0xb8, /* BYTES: 2744 */
-		0x00, 0x00, 0x00, 0x1f, /* PKTS: 31*/
-		0x0f, 0xed, 0x0a, 0xc0, /* FIRST_SWITCHED:  */
-		0x0f, 0xee, 0x18, 0x00, /* LAST_SWITCHED: */
-	},
-};
+#define ENTITIES(RT,R) \
+	RT(IPV4_SRC_ADDR, 4, 0, 10, 13, 122, 44) \
+	RT(IPV4_DST_ADDR, 4, 0, 66, 220, 152, 19) \
+	RT(IP_PROTOCOL_VERSION, 1, 0, 4) \
+	RT(PROTOCOL, 1, 0, 6) \
+	RT(L4_SRC_PORT, 2, 0, UINT16_TO_UINT8_ARR(54713)) \
+	RT(L4_DST_PORT, 2, 0, UINT16_TO_UINT8_ARR(443)) \
+	RT(FLOW_END_REASON, 1, 0, 3) \
+	RT(BIFLOW_DIRECTION, 1, 0, 1) \
+	RT(FLOW_SAMPLER_ID, 1, 0, 0) \
+	RT(TRANSACTION_ID, 8, 0, 0x8f, 0x63, 0xf3, 0x40, \
+				 0x00, 0x01, 0x00, 0x00) \
+	RT(APPLICATION_ID, 4, 0, FLOW_APPLICATION_ID(13, 459)) \
+	RT(CISCO_URL, 0xffff, 9, M_CISCO_URL) \
+	RT(CISCO_URL, 0xffff, 9, CISCO_HOST) \
+	RT(CISCO_URL, 0xffff, 9, CISCO_UA) \
+	RT(CISCO_URL, 0xffff, 9, CISCO_REFERER) \
+	RT(IN_BYTES, 8, 0, UINT64_TO_UINT8_ARR(2744)) \
+	RT(IN_PKTS, 4, 0, UINT32_TO_UINT8_ARR(31)) \
+	RT(FIRST_SWITCHED, 4, 0, 0x0f, 0xed, 0x0a, 0xc0) \
+	RT(LAST_SWITCHED, 4, 0, 0x0f, 0xee, 0x18, 0x00)
 
 static const struct checkdata_value checkdata_values1[] = {
 	{.key = "type", .value="netflowv10"},
@@ -148,6 +88,11 @@ static const struct checkdata_value checkdata_values1[] = {
 };
 
 static int prepare_test_nf10_cisco_url(void **state) {
+	static const IPFIX_TEMPLATE(v10Template, TEST_IPFIX_FLOW_HEADER,
+		IPFIX_TEMPLATE_ID, ENTITIES);
+	static const IPFIX_FLOW(v10Flow, TEST_IPFIX_FLOW_HEADER,
+		IPFIX_TEMPLATE_ID, ENTITIES);
+
 	static const struct checkdata sl1_checkdata = {
 		.checks = checkdata_values1,
 		.size = RD_ARRAYSIZE(checkdata_values1),
